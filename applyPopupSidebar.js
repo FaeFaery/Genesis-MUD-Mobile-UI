c34083a2a9ce4e8eecf270ec0fc056eb92a21ae3
FaeFaery <98637551+FaeFaery@users.noreply.github.com>
2025-03-18 01:46:48 +0800

    Update applyPopupSidebar.js

--- applyPopupSidebar.js
+++ applyPopupSidebar.js
@@ -6,15 +6,12 @@ function injectMinifiedCSS() {
 
 function injectMinifiedHTML() {
     const container = document.createElement('div');
-    const popupNav = document.getElementById('popupnav-container');
-    if (popupNav) return;
     container.innerHTML = `<link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" /><div id="popupnav-container"><span class='map-icon material-symbols-outlined'>explore</span><div class='chat-icon-container'><span class='chat-icon material-symbols-outlined'>chat_bubble</span></div></div><div id="mobile-nav"><div class='resizable popup-map'><div id="popup-buttons"><span class='exit-icon material-symbols-outlined'>close</span><span class='center-icon material-symbols-outlined'>center_focus_strong</span><div class='chat-icon-container'><span class='chat-icon material-symbols-outlined'>chat_bubble</span></div></div><div class="zoom-controls"><button id="zoom-in" class="zoom-btn">+</button><input type="range" id="zoom-slider" class="zoom-slider" min="100" max="250" value="100" style="writing-mode: vertical-lr; direction: rtl"><button id="zoom-out" class="zoom-btn">−</button></div><div class='resizers'><canvas id='magicmap-canvas'></canvas><div class='resizer top-left'></div><div class='resizer top-right'></div><div class='resizer bottom-left'></div><div class='resizer bottom-right'></div></div></div><div class='resizable popup-chat'><div id="popup-buttons"><span class='exit-icon material-symbols-outlined'>close</span><span class='map-icon material-symbols-outlined'>explore</span></div><div class='resizers'><div class='chat-content'></div><div class='resizer bottom-left'></div><div class='resizer bottom-right'></div></div></div></div>`;
-
     document.body.appendChild(container);
 }
 
 function positionPopupNav() {
-    const popupNav = document.getElementById("popup-nav-container");
+    const popupNav = document.getElementById("popupnav-container");
     const mudOutput = document.getElementById("mudoutput");
 
     if (!popupNav || !mudOutput) return;
